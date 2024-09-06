c1042f1b2cc455ee2825403cc34ce242b247f500
saquib34 <shadmanshahin6@gmail.com>
2024-09-06 15:34:52 +0530

    payment

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
   plugins: [react()],
   define: {
     'import.meta.env.VITE_APP_EASEBUZZ_LINK': JSON.stringify(process.env.VITE_APP_EASEBUZZ_LINK),
     'import.meta.env.VITE_EASEBUZZ_KEY': JSON.stringify(process.env.VITE_EASEBUZZ_KEY),
   },
-})
+});
