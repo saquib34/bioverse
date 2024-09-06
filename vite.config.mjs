6845ef5a6c96346e47ab98810e5a8ef4af76ec63
saquib34 <shadmanshahin6@gmail.com>
2024-09-06 12:46:47 +0530

    payment

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -9,14 +10,5 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://pay.easebuzz.in',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
   }
 });
